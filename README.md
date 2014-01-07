da6992d7c7cb54f1bf95b9ac6be7a1a04c6e4877
Mario César Señoranis Ayala <mariocesar@creat1va.com>
2014-01-07 18:51:36 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+django-icalendar
+================
+
+django-icalendar store events and serve iCalendar files following the standard in RFC 2445.
+
+Installation
+------------
+
+```
+pip install django-icalendar
+```
---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf8 -*-
+
+from __future__ import unicode_literals
+from setuptools import setup, find_packages
+
+
+setup(
+    name='django-icalendar',
+    version='0.1',
+    author='Mario César Señoranis Ayala',
+    author_email='mariocesar@c1.com.bo',
+    description='django-icalendar store events and serve iCalendar files following the standard in RFC 2445.',
+    packages=find_packages(),
+)
